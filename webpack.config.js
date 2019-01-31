269fddf6981bac3316938292429d4d759f6da468
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-01-31 10:43:59 -0700

    ---

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: './src/scripts/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, '/dist')
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/',
     }
 };
